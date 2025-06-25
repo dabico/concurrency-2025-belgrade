606254926734a77250c7f386101b11e3b619f124
Ozren Dabić <ozren.dabic@jetbrains.com>
2025-06-25 23:43:01 +0200

    Address TODOs in `FAABasedQueue`

--- src/day2/FAABasedQueue.kt
+++ src/day2/FAABasedQueue.kt
@@ -3,27 +3,94 @@ package day2
 import day1.*
 import java.util.concurrent.atomic.*
 
-// TODO: Copy the code from `FAABasedQueueSimplified`
-// TODO: and implement the infinite array on a linked list
-// TODO: of fixed-size `Segment`s.
 class FAABasedQueue<E> : Queue<E> {
+
+    private val head: AtomicReference<Segment>
+    private val tail: AtomicReference<Segment>
+
+    private val enqIdx = AtomicLong(0)
+    private val deqIdx = AtomicLong(0)
+
+    init {
+        val sentinel = Segment(id = 0)
+        head = AtomicReference(sentinel)
+        tail = AtomicReference(sentinel)
+    }
+
+    private val shouldTryDequeue get(): Boolean {
+        while (true) {
+            val deq = deqIdx.get().toInt()
+            val enq = enqIdx.get().toInt()
+            if (deq != deqIdx.get().toInt()) continue
+            return deq < enq
+        }
+    }
+
+    private fun moveHeadForward(segment: Segment) {
+        val current = head.get()
+        if (current.id < segment.id)
+            head.compareAndSet(current, segment)
+    }
+
+    private fun moveTailForward(segment: Segment) {
+        val current = tail.get()
+        if (current.id < segment.id)
+            tail.compareAndSet(current, segment)
+    }
+
+    private fun findSegment(start: Segment, id: Long): Segment {
+        var current = start
+        while (true) {
+            if (current.id == id) return current
+            val next = current.next.get()
+            if (next != null) {
+                current = next
+            } else {
+                val created = Segment(id = current.id + 1)
+                if (current.next.compareAndSet(null, created)) {
+                    if (created.id == id) return created
+                    current = created
+                }
+            }
+        }
+    }
+
     override fun enqueue(element: E) {
-        TODO("implement me")
+        while (true) {
+            val current = tail.get()
+            val idx = enqIdx.getAndIncrement().toInt()
+            val segment = findSegment(
+                start = current,
+                id = idx.toLong() / SEGMENT_SIZE,
+            )
+            moveTailForward(segment)
+            if (segment.compareAndSet(idx % SEGMENT_SIZE, null, element))
+                return
+        }
     }
 
     @Suppress("UNCHECKED_CAST")
     override fun dequeue(): E? {
-        TODO("implement me")
+        while (true) {
+            if (!shouldTryDequeue) return null
+            val current = head.get()
+            val idx = deqIdx.getAndIncrement().toInt()
+            val segment = findSegment(
+                start = current,
+                id = idx.toLong() / SEGMENT_SIZE,
+            )
+            moveHeadForward(segment)
+            if (segment.compareAndSet(idx % SEGMENT_SIZE, null, POISONED))
+                continue
+            return segment.get(idx % SEGMENT_SIZE) as E
+        }
     }
 }
 
-// TODO: Use me to construct a linked list of segments.
 private class Segment(val id: Long) : AtomicReferenceArray<Any?>(SEGMENT_SIZE) {
     val next = AtomicReference<Segment?>(null)
 }
 
-// TODO: Use me to mark a cell poisoned.
 private val POISONED = Any()
 
-// DO NOT CHANGE THIS CONSTANT
 private const val SEGMENT_SIZE = 2
