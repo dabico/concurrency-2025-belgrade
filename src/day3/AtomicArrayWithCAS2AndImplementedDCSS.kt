2c8b494510efd0a055938042df57faa664df4c9b
Ozren Dabić <ozren.dabic@jetbrains.com>
2025-06-27 00:43:12 +0200

    Address TODOs in `AtomicArrayWithCAS2AndImplementedDCSS`

--- src/day3/AtomicArrayWithCAS2AndImplementedDCSS.kt
+++ src/day3/AtomicArrayWithCAS2AndImplementedDCSS.kt
@@ -16,44 +16,111 @@ class AtomicArrayWithCAS2AndImplementedDCSS<E : Any>(size: Int, initialValue: E)
         }
     }
 
-    fun get(index: Int): E {
-        // TODO: Copy the implementation from `AtomicArrayWithCAS2Simplified`
-        TODO("Implement me")
+    fun get(index: Int): E = when (val element = array.get(index)) {
+        is AtomicArrayWithCAS2AndImplementedDCSS<*>.CAS2Descriptor -> element.get(index) as E
+        else -> element as E
     }
 
     fun cas2(
         index1: Int, expected1: E, update1: E,
-        index2: Int, expected2: E, update2: E
+        index2: Int, expected2: E, update2: E,
     ): Boolean {
         require(index1 != index2) { "The indices should be different" }
-        val descriptor = if (index1 < index2) {
-            CAS2Descriptor(
+        val descriptor = when {
+            index1 < index2 -> CAS2Descriptor(
                 index1 = index1, expected1 = expected1, update1 = update1,
-                index2 = index2, expected2 = expected2, update2 = update2
+                index2 = index2, expected2 = expected2, update2 = update2,
             )
-        } else {
-            CAS2Descriptor(
+            else -> CAS2Descriptor(
                 index1 = index2, expected1 = expected2, update1 = update2,
-                index2 = index1, expected2 = expected1, update2 = update1
+                index2 = index1, expected2 = expected1, update2 = update1,
             )
         }
-        descriptor.apply()
-        return descriptor.status.get() === SUCCESS
+        return with(descriptor) {
+            tryInstallDescriptorInIndex1()
+            status.get() == SUCCESS
+        }
     }
 
     inner class CAS2Descriptor(
-        val index1: Int,
-        val expected1: E,
-        val update1: E,
-        val index2: Int,
-        val expected2: E,
-        val update2: E
+        val index1: Int, val expected1: E, val update1: E,
+        val index2: Int, val expected2: E, val update2: E,
     ) {
         val status = AtomicReference(UNDECIDED)
 
-        fun apply() {
-            // TODO: Copy the implementation from `AtomicArrayWithCAS2Simplified`
-            // TODO: and use `dcss(..)` to install the descriptor.
+        fun tryInstallDescriptorInIndex1() {
+            while (true) {
+                when (val value1 = array.get(index1)) {
+                    is AtomicArrayWithCAS2AndImplementedDCSS<*>.CAS2Descriptor -> {
+                        value1.tryInstallDescriptorInIndex2()
+                        value1.applyLogicallyAndPhysically()
+                    }
+
+                    else -> {
+                        if (value1 != expected1) {
+                            status.set(FAILED)
+                            return
+                        }
+
+                        if (!dcss(index1, expected1, this, status, UNDECIDED)) continue
+
+                        tryInstallDescriptorInIndex2()
+                        applyLogicallyAndPhysically()
+                        return
+                    }
+                }
+            }
+        }
+
+        private fun tryInstallDescriptorInIndex2() {
+            while (true) {
+                when (val value2 = array.get(index2)) {
+                    is AtomicArrayWithCAS2AndImplementedDCSS<*>.CAS2Descriptor -> {
+                        if (value2 == this) return
+                        value2.tryInstallDescriptorInIndex2()
+                        value2.applyLogicallyAndPhysically()
+                    }
+
+                    else -> {
+                        if (value2 != expected2) {
+                            status.compareAndSet(UNDECIDED, FAILED)
+                            return
+                        }
+
+                        if (status.get() != UNDECIDED) return
+                        if (dcss(index2, expected2, this, status, UNDECIDED)) return
+                    }
+                }
+            }
+        }
+
+        private fun applyLogicallyAndPhysically() {
+            status.compareAndSet(UNDECIDED, SUCCESS)
+            when (val status = status.get()) {
+                SUCCESS -> {
+                    array.compareAndSet(index2, this, update2)
+                    array.compareAndSet(index1, this, update1)
+                }
+                FAILED -> {
+                    array.compareAndSet(index2, this, expected2)
+                    array.compareAndSet(index1, this, expected1)
+                }
+                else -> error("Invalid status: $status")
+            }
+        }
+
+        fun get(index: Int): E {
+            val unsuccessful = when (val status = status.get()) {
+                SUCCESS -> false
+                FAILED, UNDECIDED -> true
+                else -> error("Invalid status: $status")
+            }
+
+            return when (index) {
+                index1 -> if (unsuccessful) expected1 else update1
+                index2 -> if (unsuccessful) expected2 else update2
+                else -> error("Invalid index: $index")
+            }
         }
     }
 
@@ -61,8 +128,6 @@ class AtomicArrayWithCAS2AndImplementedDCSS<E : Any>(size: Int, initialValue: E)
         UNDECIDED, SUCCESS, FAILED
     }
 
-    // TODO: Please use this DCSS implementation to ensure that
-    // TODO: the status is `UNDECIDED` when installing the descriptor.
     fun dcss(
         index: Int,
         expectedCellState: Any?,
