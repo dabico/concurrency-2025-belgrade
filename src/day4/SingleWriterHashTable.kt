e669081f917610ecc43e62384387837244d5a420
Ozren Dabić <ozren.dabic@jetbrains.com>
2025-06-29 22:55:56 +0200

    Address TODOs in `SingleWriterHashTable`

--- src/day4/SingleWriterHashTable.kt
+++ src/day4/SingleWriterHashTable.kt
@@ -59,18 +59,15 @@ class SingleWriterHashTable<K : Any, V : Any>(initialCapacity: Int) : HashTable<
             // If neither the key nor an empty cell is found,
             // inform the caller that the table should be resized.
             repeat(MAX_PROBES) {
-                // Read the key.
                 val curKey = keys[index]
                 when (curKey) {
-                    // The cell contains the specified key.
-                    key -> {
+                    key -> { // The cell contains the specified key.
                         // Update the value and return the previous one.
                         val oldValue = values[index]
                         values[index] = value
                         return oldValue
                     }
-                    // The cell does not store a key.
-                    null, REMOVED_KEY -> {
+                    null, REMOVED_KEY -> { // The cell does not store a key.
                         // Insert the key/value pair into this cell.
                         keys[index] = key
                         values[index] = value
@@ -91,22 +88,15 @@ class SingleWriterHashTable<K : Any, V : Any>(initialCapacity: Int) : HashTable<
             // If neither the key is not found after that,
             // the table does not contain it.
             repeat(MAX_PROBES) {
-                // Read the key.
                 val curKey = keys[index]
                 when (curKey) {
                     // The cell contains the required key.
-                    key -> {
-                        // Read the value associated with the key.
-                        return values[index]
-                    }
-                    // Empty cell.
-                    null -> {
-                        // The key has not been found.
-                        return null
-                    }
+                    key -> return values[index]
+                    // Empty cell. The key has not been found.
+                    null -> return null
+                    // Process the next cell, use linear probing.
+                    else -> index = (index + 1) % capacity
                 }
-                // Process the next cell, use linear probing.
-                index = (index + 1) % capacity
             }
             // The key has not been found.
             return null
@@ -118,33 +108,15 @@ class SingleWriterHashTable<K : Any, V : Any>(initialCapacity: Int) : HashTable<
             // If neither the key is not found after that,
             // the table does not contain it.
             repeat(MAX_PROBES) {
-                // Read the key.
-                val curKey = keys[index]
+                val curKey = keys.get(index)
                 when (curKey) {
                     // The cell contains the required key.
-                    key -> {
-                        // TODO: Once a table cell is associated with a key,
-                        // TODO: it should be associated with it forever.
-                        // TODO: This way, `remove()` should only set `null` to the value slot,
-                        // TODO: without replacing the key slot with `REMOVED_KEY`.
-
-                        // Mark the slot available for `put(..)`,
-                        // but do not stop on this cell when searching for a key.
-                        // For that, replace the key with `REMOVED_KEY`.
-                        keys[index] = REMOVED_KEY
-                        // Read the value associated with the key and replace it with `null`.
-                        val oldValue = values[index]
-                        values[index] = null
-                        return oldValue
-                    }
-                    // Empty cell.
-                    null -> {
-                        // The key has not been found.
-                        return null
-                    }
+                    key -> return values.getAndSet(index, null)
+                    // Empty cell. The key has not been found.
+                    null -> return null
+                    // Process the next cell, use linear probing.
+                    else -> index = (index + 1) % capacity
                 }
-                // Process the next cell, use linear probing.
-                index = (index + 1) % capacity
             }
             // The key has not been found.
             return null
@@ -157,9 +129,4 @@ class SingleWriterHashTable<K : Any, V : Any>(initialCapacity: Int) : HashTable<
 private const val MAGIC = -0x61c88647 // golden ratio
 private const val MAX_PROBES = 2 // DO NOT CHANGE THIS CONSTANT
 private val NEEDS_RESIZE = Any()
-
-// TODO: Once a table cell is associated with a key,
-// TODO: it should be associated with it forever.
-// TODO: This way, `remove()` should only set `null` to the value slot,
-// TODO: without replacing the key slot with `REMOVED_KEY`.
 private val REMOVED_KEY = Any()
